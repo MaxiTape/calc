76e96f0427e14577b6e07200be909b16ed1c1f3e
MaxiTape <mistr.riflere@gmail.com>
2021-02-15 21:05:37 +0300

    Init, Add new Class "Test"

--- src/com/maxitape/Test.java
+++ src/com/maxitape/Test.java
@@ -1,8 +1,7 @@
 package com.maxitape;
 
-public class Main {
-
+public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Test Class");
     }
 }
